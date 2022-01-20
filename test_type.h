3e42001848bf9c4eca0ae88465c07d043087f5f1
ZhekLu <justletmego@list.ru>
2022-01-20 15:00:32 +0300

    Custom type for tests

---
+++ test_type.h
@@ -0,0 +1,44 @@
+#pragma once
+
+class TestType {
+public:
+    static int AliveCount() {
+        return count_alive;
+    }
+
+    TestType() {
+        this->value = 0;
+        ++count_alive;
+    }
+
+    TestType(int value) {
+        this->value = value;
+        ++count_alive;
+    };
+
+    TestType(const TestType& other) : value(other.value) {
+        ++count_alive;
+    }
+
+    TestType(TestType&&) = delete;
+
+    TestType& operator=(const TestType& other) {
+        this->value = other.value;
+        ++count_alive;
+        return *this;
+    }
+
+    TestType& operator=(TestType&& other) = delete;
+
+    ~TestType() {
+        --count_alive;
+    }
+
+    bool operator==(int other) const {
+        return value == other;
+    }
+
+private:
+    int value;
+    static inline int count_alive = 0;
+};
